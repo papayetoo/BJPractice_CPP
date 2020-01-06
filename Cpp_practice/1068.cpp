681cacf1633a68ea5cd95659ae657f5d1eaf945c
papayetoo <rhkdgus0826@gmail.com>
2020-01-06 17:51:58 +0900

    DP 1

--- Cpp_practice/1068.cpp
+++ Cpp_practice/1068.cpp
@@ -5,65 +5,55 @@
 //  Created by 최광현 on 2020/01/05.
 //  Copyright © 2020 최광현. All rights reserved.
 //
-
-#include <iostream>
-#define MAX 50
-using namespace std;
-
-void solution(int arr[], int n, int deleteNode);
-int isLeaf(int arr[], int n);
-
-int main(){
-    
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    
-    int n; cin >> n;
-    
-    int arr[MAX];
-    for(int i =0; i < n; i++){
-        cin >> arr[i];
-    }
-    
-    int deleteNode; cin >> deleteNode;
-    
-    solution(arr, n, deleteNode);
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
-    }
-    cout << "\n";
-    cout << isLeaf(arr, n);
-    return 0;
-}
-
-void solution(int arr[], int n, int deleteNode){
-    
-    if(!deleteNode)
-        return;
-    
-    arr[deleteNode] = -1;
-    for(int i = deleteNode + 1; i < n; i++){
-        if(arr[i] == deleteNode)
-            solution(arr, n, i);
-    }
-    
-}
-
-int isLeaf(int arr[], int n){
-    
-    int ret = 0;
-    
-    for(int i = 0; i < n -1; i++){
-        
-        int count = 0;
-        for(int j = i + 1; j < n ; j++){
-            if(arr[j] != -1 && arr[j] == i)
-                count++;
-        }
-        
-        if(!count)
-            ret++;
-    }
-
-    return ret;
-}
+//
+//#include <iostream>
+//#define MAX 50
+//using namespace std;
+//
+//struct Node{
+//    int parent;
+//    int child_count = 0;
+//};
+//
+//void change(Node* node, int size, int delNum);
+//
+//int main(){
+//
+//    ios::sync_with_stdio(false);
+//    cin.tie(0);
+//
+//    int n; cin >> n;
+//
+//    Node node[MAX];
+//
+//    for(int i =0; i < n; i++){
+//        cin >> node[i].parent;
+//    }
+//    for(int i = 0; i < n; i++){
+//        if(node[i].parent != -1)
+//            node[node[i].parent].child_count++;
+//    }
+//
+//    int delNum; cin >> delNum;
+//    change(node, n, delNum);
+//
+//    int count = 0;
+//    for(int i = 0; i < n; i++){
+//        if(node[i].parent != -2 && node[i].child_count == 0)
+//            count++;
+//    }
+//    cout << count << endl;
+//
+//}
+//
+//void change(Node* node, int size, int delNum){
+//
+//
+//    node[node[delNum].parent].child_count--;
+//    node[delNum].parent = -2;
+//    for(int i = 0; i < size; i++){
+//        if(node[i].parent == delNum)
+//            change(node, size, i);
+//    }
+//
+//}
--- Cpp_practice/1252.cpp
+++ Cpp_practice/1252.cpp
@@ -1,120 +1,104 @@
-    //
-    //  1252.cpp
-    //  Cpp_practice
-    //
-    //  Created by 최광현 on 2020/01/06.
-    //  Copyright © 2020 최광현. All rights reserved.
-    //
-
-#include <iostream>
-#include <bitset>
-#include <string>
-#define MAX 83
-
-using namespace std;
-
-string solution(string longer, string shorter);
-
-int main(){
-    
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    
-    string a = "", b= "";
-    for(int i = 0; i < 65; i++){
-        a += "1";
-        if(i%5 == 0 || i%5 == 3)
-            b += "0";
-        else
-            b += "1";
-    }
-    
-    cout << a << " " << b << endl;
-    
-    cout << "My answer : ";
-    if(a.length() >= b.length())
-        cout << solution(a, b)<<"\n";
-    else
-        cout << solution(b, a)<<"\n";
-    
-    bitset<MAX> bitA(a);
-    bitset<MAX> bitB(b);
-    unsigned long long numA = bitA.to_ullong();
-    unsigned long long numB = bitB.to_ullong();
-    unsigned long long result = numA + numB;
-    
-    bitset<MAX> bitResult(result);
-    cout << "Correct: ";
-    if(!result)
-        cout << 0 << "\n";
-    else{
-        int i = MAX - 1;
-        while(!bitResult[i]){
-            i--;
-        }
-        for(; i>=0;i--){
-            cout << bitResult[i];
-        }
-        cout << "\n";
-    }
-    
-    string resultStr = bitResult.to_string();
-    return 0;
-}
-
-string solution(string longer, string shorter){
-    
-    string result = "";
-    int carry = 0;
-    
-    size_t llength = longer.length();
-    size_t slength = shorter.length();
-    
-    if(llength > slength){
-        for(size_t j = 0; j < llength - slength; j++){
-            shorter.insert(shorter.begin(), '0');
-        }
-    }
-    
-    for(int i = llength - 1; i >= 0; i--){
-        
-        if(longer[i] == '1' && shorter[i] == '1')
-        {
-            if(!carry)
-            {
-                result.insert(result.begin(), '0');
-                carry  = 1;
-            }
-            else{
-                result.insert(result.begin(), '1');
-                carry = 1;
-            }
-        }else if(longer[i] == '1' && shorter[i] == '0'){
-            if(!carry)
-                result.insert(result.begin(), '1');
-            else{
-                result.insert(result.begin(), '0');
-                carry = 1;
-            }
-        }else if(longer[i] == '0' && shorter[i] == '1'){
-            if(!carry)
-                result.insert(result.begin(), '1');
-            else{
-                result.insert(result.begin(), '0');
-                carry = 1;
-            }
-        }else{
-            if(!carry)
-                result.insert(result.begin(), '0');
-            else{
-                result.insert(result.begin(), '1');
-                carry = 0;
-            }
-        }
-    }
-    
-    if(result.length() < 80 && carry)
-        result.insert(result.begin(), '1');
-    
-    return result;
-}
+//        //
+//        //  1252.cpp
+//        //  Cpp_practice
+//        //
+//        //  Created by 최광현 on 2020/01/06.
+//        //  Copyright © 2020 최광현. All rights reserved.
+//        //
+//
+//    #include <iostream>
+//    #include <bitset>
+//    #include <string>
+//    #define MAX 80
+//
+//    using namespace std;
+//
+//    string solution(string longer, string shorter);
+//
+//    int main(){
+//        
+//        ios_base::sync_with_stdio(false);
+//        cin.tie(0);
+//        
+//        string a, b;
+//        cin >> a >> b;
+//        
+//        string result;
+//        if(a.length() >= b.length())
+//            result = solution(a, b);
+//        else
+//            result = solution(b, a);
+//        
+//        if(result.find("1") == string::npos){
+//            cout << 0 << endl;
+//        }else{
+//            int i = 0;
+//            while(result[i] == '0'){
+//                i++;
+//            }
+//            for(;i < result.length();i++){
+//                cout << result[i];
+//            }
+//        }
+//        
+//        
+//        return 0;
+//    }
+//
+//    string solution(string longer, string shorter){
+//        
+//        string result = "";
+//        int carry = 0;
+//        
+//        int llength = longer.length();
+//        int slength = shorter.length();
+//        
+//        if(llength > slength){
+//            for(int j = 0; j < llength - slength; j++){
+//                shorter.insert(shorter.begin(), '0');
+//            }
+//        }
+//        
+//        for(int i = longer.length() - 1; i >= 0; i--){
+//            
+//            if(longer[i] == '1' && shorter[i] == '1')
+//            {
+//                if(!carry)
+//                {
+//                    result.insert(result.begin(), '0');
+//                    carry  = 1;
+//                }
+//                else{
+//                    result.insert(result.begin(), '1');
+//                    carry = 1;
+//                }
+//            }else if(longer[i] == '1' && shorter[i] == '0'){
+//                if(!carry)
+//                    result.insert(result.begin(), '1');
+//                else{
+//                    result.insert(result.begin(), '0');
+//                    carry = 1;
+//                }
+//            }else if(longer[i] == '0' && shorter[i] == '1'){
+//                if(!carry)
+//                    result.insert(result.begin(), '1');
+//                else{
+//                    result.insert(result.begin(), '0');
+//                    carry = 1;
+//                }
+//            }else{
+//                if(!carry)
+//                    result.insert(result.begin(), '0');
+//                else{
+//                    result.insert(result.begin(), '1');
+//                    carry = 0;
+//                }
+//            }
+//        }
+//        
+//        if(carry)
+//            result.insert(result.begin(), '1');
+//        
+//        return result;
+//    }
+//
