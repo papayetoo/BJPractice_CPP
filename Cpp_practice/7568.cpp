7cbc1aa21288300eb618fcc678bc91d9b817777b
papayetoo <33744981+papayetoo@users.noreply.github.com>
2020-01-10 18:08:48 +0900

    Update 7568.cpp
    주석추가

--- Cpp_practice/7568.cpp
+++ Cpp_practice/7568.cpp
@@ -30,21 +30,21 @@ int main(){
        cin >> arr[i].weight >> arr[i].height;
    }
    
-   cout << "Test 2 \n";
-   cout << "what's the difference between commit and push?\n";
-   
    for(int i = 0; i < n; i++){
        int same = 0, bigger = 0, lower = 0;
        int j = (i + 1) % n;
        while(j% n != i){
+          //arr[i]를 보다 큰 arr[j]를 만나면 bigger++
            if(arr[i].weight < arr[j].weight && arr[i].height < arr[j].height)
                bigger++;
+          //arr[i]보다 weight과 height 모두 작을 때 lower++
            else if(arr[i].weight > arr[j].weight && arr[i].height > arr[j].height)
                lower++;
            else
                same++;
            j = (j + 1) % n;
        }
+      //등수 계산은 전체 n에서 작거나 같은 것을 빼면 출력 가능.
        cout << n - lower - same << " ";
    }
    
